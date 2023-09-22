7e6240d0454ecf0f9c191debd52d873d74b664f6
Chris <who7708@163.com>
2023-09-22 20:33:58 +0800

    解释了为啥可以不添加 @Mapper 注解,也可以解析出来对应的mapperJava 接口

--- src/test/java/org/mybatis/my/mapper/UserMapper.java
+++ src/test/java/org/mybatis/my/mapper/UserMapper.java
@@ -1,11 +1,16 @@
 package org.mybatis.my.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.mybatis.my.model.User;
 
-@Mapper
+// @Mapper 注解主要是在
+// <mybatis-spring:scan annotation="org.apache.ibatis.annotations.Mapper"
+// 或
+// ClassPathMapperScanner#setAnnotationClass(Mapper.class)
+// 在这两个地方使用的.也可以不添加此注解
+// @Mapper
+// @Component
 public interface UserMapper {
 
     @Select("select * from t_user where name = #{name}")
